6c48cb836feaa1661971016548a4cda4654ee9e3
Menezes Pinto, Michel <michel.pinto@gft.com>
2025-09-26 20:05:04 -0300

    gh-pages configs

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: mode === "production" ? "/ozonated-natural-reach/" : "/",
+  base: "/ozonated-natural-reach/",
 }));
